f1704fdaf9e9a4477c1d7785ae36194168cf293c
jatin mittal <jatinmittal2651@gmail.com>
2019-05-01 13:06:38 +1000

    fix checkstyle warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -17,7 +17,8 @@ public class Greeter {
 *@return greeting string
 */
   
-  public String greet(final String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
+
